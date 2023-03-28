aa262828b7c2a95a01d9afd77b73f35afe7ddcb4
Mark <honhanphuoc@gmail.com>
2023-03-28 13:39:41 +0700

    Push GioHang.cs

---
+++ ASP_NET_MVC/Models/GioHang.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASP_NET_MVC.Models
+{
+    [Serializable]
+    public class GioHang
+    {
+        public San_Pham San_Pham { get; set; }
+        public int Quantity { get; set; }
+
+    }
+}
