c6dd20ee04e84b5525da1b9454aa5d5d6f9ead51
Mark <honhanphuoc@gmail.com>
2023-03-28 13:31:56 +0700

    Push GioHangController

--- ASP_NET_MVC/Controllers/HomeController.cs
+++ ASP_NET_MVC/Controllers/HomeController.cs
@@ -1,29 +1,127 @@
-﻿using System;
+﻿using PagedList;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
+using ASP_NET_MVC.Models;
 
 namespace ASP_NET_MVC.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        // GET: Home
+        QuanLyBanHoaEntities db = new QuanLyBanHoaEntities();
+        public ActionResult Index(int page = 1, int Pagesize = 8)
+        {
+            return View(db.San_Pham.ToList().ToPagedList(page, Pagesize));
+        }
+
+        public ActionResult Details(int id)
+        {
+            San_Pham sanpham = db.San_Pham.SingleOrDefault(n => n.id == id);
+            return View(sanpham);
+        }
+        [HttpGet]
+        // Register
+        public ActionResult Register()
         {
             return View();
         }
-
-        public ActionResult About()
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(Nguoi_Dung user)
         {
-            ViewBag.Message = "Your application description page.";
-
+            if (ModelState.IsValid)
+            {
+                var checkmail = db.Nguoi_Dung.FirstOrDefault(n => n.email == user.email);
+                var checktaikhoan = db.Nguoi_Dung.FirstOrDefault(n => n.taikhoan == user.taikhoan);
+                if (checkmail == null && checktaikhoan == null)
+                {
+                    db.Configuration.ValidateOnSaveEnabled = false;
+                    db.Nguoi_Dung.Add(user);
+                    db.SaveChanges();
+                    return RedirectToAction("DangKyThanhCong");
+                }
+                else if (checktaikhoan != null)
+                {
+                    ViewBag.Error = " Tài khoản đã tồn tại";
+                    return View();
+                }
+                else
+                {
+                    ViewBag.Errorr = " Email đã tồn tại";
+                    return View();
+                }
+            }
             return View();
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
 
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(string taikhoan, string matkhau)
+        {
+            if (ModelState.IsValid)
+            {
+                var tk = db.Nguoi_Dung.Where(n => n.taikhoan.Equals(taikhoan));
+                var mk = db.Nguoi_Dung.Where(n => n.matkhau.Equals(matkhau));
+                if (tk.Count() > 0 && mk.Count() > 0)
+                {
+                    Session["ten"] = tk.FirstOrDefault().ten;
+                    Session["taikhoan"] = tk.FirstOrDefault().taikhoan;
+                    Session["email"] = tk.FirstOrDefault().email;
+                    Session["SDT"] = tk.FirstOrDefault().SDT;
+                    Session["diachi"] = tk.FirstOrDefault().diachi;
+                    Session["quyen"] = tk.FirstOrDefault().quyen;
+                    Session["id"] = tk.FirstOrDefault().id;
+                    if (Session["quyen"] != null)
+                    {
+                        return Redirect("~/Admin/Home/Index");
+                    }
+                    else
+                    {
+                        return RedirectToAction("Index");
+                    }
+
+                }
+                else if (tk.Count() > 0 || mk.Count() < 0)
+                {
+                    ViewBag.errormk = "Mật khẩu không đúng";
+                    return View();
+                }
+                else if (tk.Count() < 0 || mk.Count() > 0)
+                {
+                    ViewBag.errortk = "Tài khoản không đúng";
+                    return View();
+                }
+                else
+                {
+                    ViewBag.errortk = "Tài khoản không đúng";
+                    ViewBag.errormk = "Mật khẩu không đúng";
+                    return View();
+                }
+
+            }
+            return View();
+
+
+        }
+
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            return RedirectToAction("Login");
+        }
+        public ActionResult DangKyThanhCong()
+        {
             return View();
         }
     }
